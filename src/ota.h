4195ffe98524e5b74918691f2dff143cb9049307
anno73 <anno@opossum.htu.tuwien.ac.at>
2021-01-01 12:24:54 +0100

    Comment on pwd length

--- src/ota.h
+++ src/ota.h
@@ -2,7 +2,7 @@
 
 namespace ota {
 
-const uint8_t OTA_UPDATE_PASWORD_STR_LEN = 40;
+const uint8_t OTA_UPDATE_PASWORD_STR_LEN = 40;  // Must be smaller than IOTWEBCONF_PASSWORD_LEN 
 extern char otaUpdatePassword[];
 
 extern void setupArduinoOta();
